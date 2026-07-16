02fdd2e7460e3b1a9b76fa06656aa7999c7e2276
Your Name <dciphererz@gmail.com>
2026-07-16 14:16:59 +0100

    track app folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-        
+    <div className='"bg-blue-800'>
+        <h1 className=''>klkdvvms,c.</h1>
+        <p >klkdvvms,c. </p>
+        <p className='font-bold'>klkdvvms,c.</p>
     </div>
   )
 }
